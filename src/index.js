6c90bb38e7f18e2dcdd2ff4347c032ca57d29a06
David <davidsantander9@gmail.com>
2021-07-13 13:03:04 -0500

    Callback hell

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import './styles.css';
 import { buscarHeroe } from './js/callbacks';
 
 const heroeId = 'capi';
+const heroeId2 = 'iron';
+const heroeId3 = 'spider';
   
 
-buscarHeroe( heroeId, (err ,heroe ) => {
-    if ( err ) {
-        console.error(err);
-    } else {  
-        console.info(heroe)
-    }
+buscarHeroe( heroeId, (err , heroe1 ) => {
+    if ( err ) { return console.error( err ) }
+
+    buscarHeroe( heroeId2, ( err, heroe2 ) => {
+        if ( err ) { return console.error( err ) } 
+        buscarHeroe( heroeId3, ( err, heroe3 ) => {
+            if ( err ) { return console.error( err ) } 
+            console.log(`Enviando a ${ heroe1.nombre }, ${ heroe2.nombre } y ${ heroe3.nombre }`);
+            
+        } );
+    } );
+    
 });
